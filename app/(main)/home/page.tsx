f496fa27f2dfc43ffcc4756f7eeccf50dabb2004
Titia13 <oceanebehou25@gmail.com>
2026-07-07 17:13:33 +0000

    Update

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -5,13 +5,14 @@ import Link from "next/link";
 export default function Home() {
   return (
    <>
+   {/* le background est de cette couleur */}
       <section className="flex flex-col items-center justify-center gap-6 px-6 pt-32 pb-20 text-center">
         <h1 className="text-6xl font-extrabold text-white tracking-tight leading-tight max-w-2xl">
-                    <span>Découvrez</span> <span>FLOW</span> votre Outil de Téléchargement de fichiers
+                    <span className="text-6xl font-extrabold text-white tracking-tight leading-tight max-w-2xl">Découvrez</span> <span>FLOW</span> votre Outil de Téléchargement de fichiers
 
         </h1>
         <p className="text-lg text-gray-400 max-w-md leading-relaxed">
-          C&apos;est un mini site de gestion de livres pour apprendre Next.js.
+          mets ici un texte d&apos;accroche pour le site flow, qui explique ce que c&apos;est et pourquoi il est utile.
         </p>
         <div className="flex gap-4 mt-4">
         </div>
@@ -20,10 +21,10 @@ export default function Home() {
       <section className="flex flex-row items-center gap-16 px-16 py-20 max-w-6xl w-full">
 
         <div className="flex flex-col gap-4">
-          <div>
+          {/* <div>
           <ChartPieLabel />
 
-          </div>
+          </div> */}
           <Link
             href="/application"
             className="inline-flex items-center gap-2 text-orange-500 font-semibold hover:text-orange-400 transition-colors"
